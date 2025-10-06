ab8f4215e5ee582347beca80fcab1e6dde289652
Isabela (Dev Local) <isabelalinharessilva@gmail.com>
2025-10-06 19:00:09 -0300

    deixando em modo html

--- aula-01/server.js
+++ aula-01/server.js
@@ -4,8 +4,8 @@ const host = "127.0.0.1";
 const port = 3000;
 
 const server = createServer((request, response)=>{
-    response.writeHead(200, {'Content-Type': 'text/plain'})
-    response.end('Hello World');
+    response.writeHead(200, {'Content-Type': 'text/html'})
+    response.end('<h1>Hello World</h1> <br/> Aula Backend');
 });
 
 server.listen(port, host, ()=>{
