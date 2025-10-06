0b09bf1097348e577da13f988a0e017224d1d553
Isabela (Dev Local) <isabelalinharessilva@gmail.com>
2025-10-06 18:46:54 -0300

    conhecendo node

---
+++ aula-01/primeiro-programa.js
@@ -0,0 +1,8 @@
+console.log("Ola, sou o JS com NodeJS");
+
+const valor1 = 10;
+const valor2 = 20;
+
+const total = valor1 + valor2;
+
+console.log("Total: "+ total);
---
+++ aula-01/server.js
@@ -0,0 +1,13 @@
+const { createServer } = require ('node:http');
+
+const host = "127.0.0.1";
+const port = 3000;
+
+const server = createServer((request, response)=>{
+    response.writeHead(200, {'Content-Type': 'text/plain'})
+    response.end('Hello World');
+});
+
+server.listen(port, host, ()=>{
+    console.log(`Servidor execultando http://${host}:${port}`)
+})
