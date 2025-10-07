cbc27af8333db848cd61d41629f88c16c904ad48
Isabela (Dev Local) <isabelalinharessilva@gmail.com>
2025-10-07 09:10:54 -0300

    inicio de paginas

--- aula-02/server.js
+++ aula-02/server.js
@@ -5,9 +5,27 @@ const host = 'localhost';
 const port= 3000;
 
 const app = createServer((request,response)=>{
-    console.log("Cliente solicitou...");
-    response.writeHead(200, {'Content-type':'text/plain'});
-    return response.end("Ola nodeJS ");
+    // const url = request.url;
+    // const method = request.method;    
+    //fazer a linha de baixo ao inves das duas linhas; 
+
+    const { url , method} = request;
+ 
+    console.log("URL -", url);
+    console.log("Metodo/Verbo - ",method);
+    
+    if(url == '/'){
+        response.writeHead(200, {'Content-type':'text/plain'});
+        return response.end("Ola nodeJS, aula de nodemon");
+    }
+
+    if (url == '/produtos') {
+        response.writeHead(200, {'Content-type':'text/plain'});
+        return response.end("Listagem de produtos");
+    }
+
+    
+    
 })
 
 app.listen(port, host,()=>{
